060e6cc4f2bfc49d838663499b45feb3f8fcde74
Jessica Silvestre <jessica.nutricionista@yahoo.com.br>
2026-03-13 21:57:33 -0300

    contador inicial

---
+++ 05-contador/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const inicio = document
+
+function gerarContagem () {
+    const inicio = document.getElementById ('inicio')
+    const fim = document.getElementById ('fim')
+    const salto = document.getElementById ('salto')
+    const lista = document.getElementById ('lista')
+
+    lista.replaceChildren()
+
+      for (let i = Number(inicio.value); i <= fim.value; i += Number(salto.value) ) {
+      const itemLista = document.createElement ('li')
+
+      itemLista.textContent = i
+      lista.appendChild(itemLista)
+    }
+}
